f2135b4d976be7ed8d057915f58d810a5c288e72
RobbyArsyadani95 <jokeranony@gmail.com>
2022-01-09 23:50:40 +0700

    nyobak user iku mang error e

--- project/resources/views/admin/dashboard/toko.blade.php
+++ project/resources/views/admin/dashboard/toko.blade.php
@@ -5,59 +5,61 @@
 @section('page-title', 'Toko')
 
 @section('header-script')
-  <link rel="stylesheet" href="{{ asset('assets/vendors/simple-datatables/style.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendors/simple-datatables/style.css') }}">
 @endsection
 
 
 @section('content')
-  <section class="section">
+<section class="section">
     <div class="card">
-      <div class="card-header">
-        <div class="row">
-          <div class="col-md-8">
-            <h3>Data barang</h3>
-          </div>
-          <div class="col-md-4 text-end">
-            <a href="#" class="btn btn-success rounded-pill">Tambah data</a>
-          </div>
+        <div class="card-header">
+            <div class="row">
+                <div class="col-md-8">
+                    <h3>Data barang</h3>
+                </div>
+                <div class="col-md-4 text-end">
+                    <a href="#" class="btn btn-success rounded-pill">Tambah data</a>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="card-body">
-        <table class="table table-striped" id="table1">
-          <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>City</th>
-                <th>Status</th>
-              </tr>
-          </thead>
-          <tbody>
-            @for ($i = 0; $i < 40; $i++)
-            <tr>
-                <td>Graiden</td>
-                <td>vehicula.aliquet@semconsequat.co.uk</td>
-                <td>076 4820 8838</td>
-                <td>Offenburg</td>
-                <td>
-                    <span class="badge bg-success">Active</span>
-                </td>
-            </tr>
-            @endfor
-          </tbody>
-        </table>
-      </div>
+        <div class="card-body">
+            <table class="table table-striped" id="table1">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Toko</th>
+                        <th>Nama Pemilik</th>
+                        <th>Alamat</th>
+                        <th>No Telephone</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for ($i = 0; $i < 40; $i++) <tr>
+                        <td>1</td>
+                        <td>Toko Barokah</td>
+                        <td>Suhar</td>
+                        <td>Tumpang</td>
+                        <td>0821111111</td>
+                        <td>
+                            <span class="badge bg-success">Edit</span>
+                            <span class="badge bg-warning">Delete</span>
+                        </td>
+                        </tr>
+                        @endfor
+                </tbody>
+            </table>
+        </div>
     </div>
-  </section>
+</section>
 @endsection
 
 
 @section('footer-script')
 <script src="{{ asset('assets/vendors/simple-datatables/simple-datatables.js') }}"></script>
 <script>
-    // Simple Datatable
-    let table1 = document.querySelector('#table1');
-    let dataTable = new simpleDatatables.DataTable(table1);
+// Simple Datatable
+let table1 = document.querySelector('#table1');
+let dataTable = new simpleDatatables.DataTable(table1);
 </script>
 @endsection
--- project/resources/views/layouts/admin-dashboard.blade.php
+++ project/resources/views/layouts/admin-dashboard.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.dashboard')
 
 @section('title')
-  @yield('title')
+@yield('title')
 @endsection
 
 @section('page-title')
-  @yield('page-title')
+@yield('page-title')
 @endsection
 
 @section('sidebar')
-    @include('layouts.admin-sidebar')
+@include('layouts.admin-sidebar')
 @endsection
 
 @section('content')
-  @yield('content')
+@yield('content')
 @endsection
